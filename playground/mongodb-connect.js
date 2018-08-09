2869129bc3288d032dae93de51b60694e655b93b
Maksimilian <maxas81@gmail.com>
2018-08-09 11:53:34 +0300

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,30 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(error,client) => {
+    if(error)
+       return console.log('Unable to connect to MongoDB server');
+
+    console.log('Connected to mongo DB');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed:false
+    // }, (error,result) => {
+    //     if(error) return console.log('Unable to insert todo ',error);
+    //     console.log(JSON.stringify(result.ops,undefined,2));
+    // });
+
+    //Insert new doc into Users (name, age, location)
+
+        // db.collection('Users').insertOne({
+        //     name:'Max',
+        //     age:23,
+        //     location:'Vilnius'
+        // }, (error,result) => {
+        //     if(error) return console.log('Unable to insert user ',error);
+        //     console.log(result.ops[0]._id.getTimestamp());
+        // });
+    client.close();
+});
